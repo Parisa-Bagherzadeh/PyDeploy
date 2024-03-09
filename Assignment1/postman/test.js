49073516477bdc843bc336e1e51c8d0ad288b59d
parisa baqerzade <parisa.baqerzade@gmail.com>
2024-03-09 13:10:11 +0330

    remove .js file

--- Assignment1/postman/test.js
+++
@@ -1,9 +0,0 @@
-var settings = {
-    "url": "https://thispersondoesnotexist.com/",
-    "method": "GET",
-    "timeout": 0,
-  };
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
