c44d14e4cb475e3303df53da97cbee4910c0819e
parisa baqerzade <parisa.baqerzade@gmail.com>
2024-07-19 23:29:57 +0330

    initial commit

--- Assignment15/main.py
+++ Assignment15/main.py
@@ -7,31 +7,24 @@ app = FastAPI()
 
 @app.get("/")
 def registered_users():
-    DATABASE_URL = "postgresql://parisa:110@some-postgres:5432/postgres"
-    engine = create_engine(DATABASE_URL, echo=True)
-    SQLModel.metadata.create_all(engine)
-
- 
-
-    connection = psycopg2.connect(database="database", user="parisa", password="110", host="localhost", port=5432)
-
-    cursor = connection.cursor()
-
-    users = cursor.execute("SELECT * from portal.portal_users;")
-
-    # Fetch all rows from database
-    record = cursor.fetchall()
-
-    return {"Count of registedred users" : len(users.fetchall())} 
+    # DATABASE_URL = "postgresql://parisa:110@some-postgres:5432/postgres"
+    # engine = create_engine(DATABASE_URL, echo=True)
+    # SQLModel.metadata.create_all(engine)
+    # connection = psycopg2.connect(database="database", user="parisa", password="110", host="localhost", port=5432)
+    # cursor = connection.cursor()
+    # users = cursor.execute("SELECT * from portal.portal_users;")
+    # # Fetch all rows from database
+    # record = cursor.fetchall()
+    # return {"Count of registedred users" : len(users.fetchall())} 
 
     # with Session(engine) as db_session:
     #         statement = select(User)
 
-    # connection = sqlite3.connect("database.db")
-    # cursor = connection.cursor()
-    # users = cursor.execute("SELECT * FROM user")
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    users = cursor.execute("SELECT * FROM user")
 
-    # return {"Count of registedred users" : len(users.fetchall())} 
+    return {"Count of registedred users" : len(users.fetchall())} 
 
 
 
